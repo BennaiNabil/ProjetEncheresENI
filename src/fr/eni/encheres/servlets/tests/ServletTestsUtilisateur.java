35bd747ceb671bf619f4e0767b81157ef6ca6856
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-02-01 20:22:14 +0100

    Ajout de la Servlet qui permet de tester UtilisateurDAOJdbcImpl

---
+++ src/fr/eni/encheres/servlets/tests/ServletTestsUtilisateur.java
@@ -0,0 +1,75 @@
+package fr.eni.encheres.servlets.tests;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.UtilisateurDAO;
+
+@WebServlet("/ServletTestUtilisateur")
+public class ServletTestsUtilisateur extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter outPrintWriter = response.getWriter();
+
+		try {
+			Context context = new InitialContext();
+			// Recherche de la DataSource
+			DataSource dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
+			// Demande une connexion
+			Connection cnx = dataSource.getConnection();
+
+			// Test de la connexion (ouverte ou fermée)
+			outPrintWriter.println("Test de l'état de la connexion");
+			outPrintWriter.println("-------------------------------" + "\n");
+			outPrintWriter.println("La connexion est " + (cnx.isClosed() ? "fermée" : "ouverte") + ".\n\n\n");
+
+			// TESTS
+			UtilisateurDAO utilisateurDAO = DAOFactory.getUtilisateurDAO();
+
+			// Tests de UtilisateurDAOJdbcImpl
+
+			// selectAll
+			outPrintWriter.println("Test de la méthode selectAll()");
+			outPrintWriter.println("-------------------------------" + "\n");
+			for (Utilisateur utilisateur : utilisateurDAO.selectAll()) {
+				outPrintWriter.println(utilisateur + "\n");
+			}
+			outPrintWriter.println("\n\n\n");
+
+			// selectById
+			outPrintWriter.println("Test de la méthode selectById()");
+			outPrintWriter.println("-------------------------------" + "\n");
+			outPrintWriter.println(utilisateurDAO.selectById(4));
+
+			// Libère une connexion
+			cnx.close();
+		} catch (NamingException | SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}

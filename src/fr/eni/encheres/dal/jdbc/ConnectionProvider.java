a5f0184d67d034f75f387a058f5bb967d05ab894
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-02-01 20:19:11 +0100

    Ajout des fichiers responsables de la configuration de la connexion

---
+++ src/fr/eni/encheres/dal/jdbc/ConnectionProvider.java
@@ -0,0 +1,39 @@
+package fr.eni.encheres.dal.jdbc;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+abstract class ConnectionProvider {
+    private static DataSource dataSource;
+
+    /*
+        Portion de code statique qui sera exécutée une seule fois: au moment de l'import de la classe
+        Elle a pour but de faire le lien avec la BD.
+     */
+    static {
+        Context context;
+        try {
+            context = new InitialContext();
+            ConnectionProvider.dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
+        } catch (NamingException e) {
+            e.printStackTrace();
+            throw new RuntimeException("Impossible d'accéder à la base de données");
+        }
+    }
+
+    /**
+     * Cette méthode retourne une connection opérationnelle issue du pool de connexion
+     * vers la base de données
+     *
+     * @return connection un objet de type Connection utilisé pour se connecter au SGBDR
+     * @throws SQLException
+     */
+    public static Connection getConnection() throws SQLException {
+        Connection connection = ConnectionProvider.dataSource.getConnection();
+        return connection;
+    }
+}

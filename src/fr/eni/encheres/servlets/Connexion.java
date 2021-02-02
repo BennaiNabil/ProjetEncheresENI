c10656b28785a5cd64656c309449752ff101c22a
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-02-02 09:07:17 +0100

    Intégration de la vérification des identifiants, fonctionnelle now

--- src/fr/eni/encheres/servlets/Connexion.java
+++ src/fr/eni/encheres/servlets/Connexion.java
@@ -1,52 +1,61 @@
 package fr.eni.encheres.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.encheres.dal.DAOFactory;
+import fr.eni.encheres.dal.UtilisateurDAO;
+
 public class Connexion extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		// Revoie vers la page de connexion.
-		
+
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/PageConnexion.jsp");
 		rd.forward(request, response);
 	}
 
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		//Récupère les données de connexion de l'utilisateur (Nom et Mot de Passe).
-		
+		// Rï¿½cupï¿½re les donnï¿½es de connexion de l'utilisateur (Nom et Mot de Passe).
+
 		String identifiant;
 		String mdp;
-		
+
 		identifiant = request.getParameter("identifiant");
 		mdp = request.getParameter("mdp");
-		
+
 		System.out.println("Identifiant : " + identifiant + " - Mot de passe : " + mdp);
-		
-		
-		//Vérifie leur conformité à la base de données.
-		// ???
-		
-			//Si oui : Redirection vers la page d'accueil Connecte. 
-		
+
+		/*
+		 * [NABIL]
+		 * 
+		 * On utilise la mÃ©thode sontBonsIdentifiantsDeConnexion pour vÃ©rifier si le
+		 * couple d'identifiant est correct
+		 */
+
+		UtilisateurDAO utilisateurDAO = DAOFactory.getUtilisateurDAO();
+		// VÃ©rifie leur conformitÃ© Ã  la base de donnÃ©es.
+		if (utilisateurDAO.sontBonsIdentifiantsDeConnexion(identifiant, mdp)) {
+			// Si oui : Redirection vers la page d'accueil Connecte.
 			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/PageAccueilConnecte.jsp");
 			rd.forward(request, response);
-		
-			//Si non : Affiche un message d'erreur. 
-			// ???
-		
+		} else {
+			// Si non : Affiche un message d'erreur.
+			request.getRequestDispatcher("/WEB-INF/jsp/PageErreurConnexion.jsp").forward(request, response);
+
+		}
 
 	}
 

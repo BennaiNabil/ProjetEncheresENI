4baf9a1745c08ba81051ad699f8459b071b5cb0f
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-02-02 09:05:33 +0100

    Ajout de l'annotation @Override (bonnes pratiques)

--- src/fr/eni/encheres/servlets/Accueillir.java
+++ src/fr/eni/encheres/servlets/Accueillir.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,14 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 public class Accueillir extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/PageAccueilAnonyme.jsp");
 		rd.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
 }

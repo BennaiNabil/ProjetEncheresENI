96502019bb7b265730d492402ded7841722b01bc
Léa V. <lvangheluwe@gmail.com>
2021-02-02 21:08:51 +0100

    Creation de la méthode SelectAll dans EnchereDAOImpl - à vérifier

--- src/fr/eni/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
@@ -1,33 +1,80 @@
 package fr.eni.encheres.dal.jdbc;
 
+import fr.eni.encheres.bo.ArticleVendu;
 import fr.eni.encheres.bo.Enchere;
+import fr.eni.encheres.bo.Utilisateur;
 import fr.eni.encheres.dal.EnchereDAO;
-
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EnchereDAOJdbcImpl implements EnchereDAO {
-    @Override
-    public void insert(Enchere object) {
+	private static final String SELECT_ALL = "Select ARTICLES_VENDUS.nom_article, ARTICLES_VENDUS.description, ARTICLES_VENDUS.date_debut_encheres, ARTICLES_VENDUS.date_fin_encheres, ARTICLES_VENDUS.prix_initial, ARTICLES_VENDUS.no_utilisateur FROM ENCHERES\n"
 
-    }
+			+ "INNER JOIN ARTICLES_VENDUS on ENCHERES.no_article = ARTICLES_VENDUS.no_article;";
 
-    @Override
-    public Enchere selectById(int id) {
-        return null;
-    }
+	@Override
+	public void insert(Enchere object) {
 
-    @Override
-    public List<Enchere> selectAll() {
-        return null;
-    }
+	}
 
-    @Override
-    public void update(Enchere object) {
+	@Override
+	public Enchere selectById(int id) {
+		return null;
+	}
 
-    }
+	@Override
+	public List<Enchere> selectAll() {
+		Statement statement = null;
+		ResultSet resultSet = null;
+		List<Enchere> listeEncheres = new ArrayList<>();
+		try (Connection connection = ConnectionProvider.getConnection()) {
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(SELECT_ALL);
 
-    @Override
-    public void delete(Enchere object) {
+			while (resultSet.next()) {
+				listeEncheres.add(new Enchere(resultSet.getObject("date_enchere", LocalDate.class),
+						resultSet.getInt("montant_enchere"), (Utilisateur) resultSet.getObject("encherisseur"),
+						(ArticleVendu) resultSet.getObject("no_article")));
+			}
+		} catch (SQLException throwables) {
+			throwables.printStackTrace();
+		} finally {
+			closeResources(statement, resultSet);
+		}
+		return listeEncheres;
+	}
+	
 
-    }
+	@Override
+	public void update(Enchere object) {
+
+	}
+
+	@Override
+	public void delete(Enchere object) {
+
+	}
+
+	private void closeResources(Statement preparedStatement, ResultSet resultSet) {
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException throwables) {
+				throwables.printStackTrace();
+			}
+		}
+		if (preparedStatement != null) {
+			try {
+				preparedStatement.close();
+			} catch (SQLException throwables) {
+				throwables.printStackTrace();
+			}
+		}
+	}
 }

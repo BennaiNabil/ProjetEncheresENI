6375050f23a901b9638dd3b6e7bf2509dd7376b5
Léa V. <lvangheluwe@gmail.com>
2021-02-02 21:10:48 +0100

    Servlet accueullir - ajout "affichage des Enchères" - à vérifier

--- src/fr/eni/encheres/servlets/Accueillir.java
+++ src/fr/eni/encheres/servlets/Accueillir.java
@@ -1,6 +1,7 @@
 package fr.eni.encheres.servlets;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -8,14 +9,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import fr.eni.encheres.bll.EncheresManager;
+import fr.eni.encheres.bo.Enchere;
+
 public class Accueillir extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		
+		//Affichage des Enchères
+			//1. Recherche des enchères
+		
+		
+		EncheresManager encheresManager = new EncheresManager();
+		List<Enchere> listeEncheres=null;
+		listeEncheres = encheresManager.selectAll();
+		request.setAttribute("listeEncheres", listeEncheres);
+		
+		
+		
+		//Renvoi vers la page d'accueil
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/PageAccueilAnonyme.jsp");
 		rd.forward(request, response);
+		
+		
+
 	}
 
 	@Override

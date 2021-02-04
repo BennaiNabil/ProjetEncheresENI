11fb86a1e62d4381aedd8c1ecad0b568d6a3428f
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-02-04 14:05:17 +0100

    Ajout doPost Servlet modification profil

--- src/fr/eni/encheres/servlets/ModifierMonProfil.java
+++ src/fr/eni/encheres/servlets/ModifierMonProfil.java
@@ -6,6 +6,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.encheres.bll.UtilisateurManager;
+import fr.eni.encheres.bo.Utilisateur;
 
 public class ModifierMonProfil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -22,6 +26,10 @@ public class ModifierMonProfil extends HttpServlet {
 			throws ServletException, IOException {
 
 		String pseudo, nom, prenom, email, tel, rue, codePostal, ville, mdp, conf;
+		UtilisateurManager manager = new UtilisateurManager();
+
+		HttpSession session = request.getSession();
+		Utilisateur utilisateurAModifier = (Utilisateur) session.getAttribute("utilisateur");
 
 		pseudo = request.getParameter("pseudo");
 		nom = request.getParameter("nom");
@@ -32,6 +40,14 @@ public class ModifierMonProfil extends HttpServlet {
 		codePostal = request.getParameter("codePostal");
 		ville = request.getParameter("ville");
 		mdp = request.getParameter("mdp");
+
+		Utilisateur utilisateurModifie = new Utilisateur(utilisateurAModifier.getNoUtilisateur(), pseudo, nom, prenom,
+				email, tel, rue, codePostal, ville, mdp, utilisateurAModifier.getCredit(),
+				utilisateurAModifier.isEstAdministrateur());
+
+		if (manager.validerUtilisateur(utilisateurModifie)) {
+			manager.modifierUtilisateur(utilisateurModifie);
+		}
 
 	}
 

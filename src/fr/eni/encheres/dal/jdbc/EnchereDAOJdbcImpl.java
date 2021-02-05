88bfd5086f5f9fe179f5d3098d1375b1a30a9a43
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-02-05 09:27:05 +0100

    Ajout de l'insert d'encheres dans la DAO

--- src/fr/eni/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/jdbc/EnchereDAOJdbcImpl.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.encheres.bo.Enchere;
@@ -15,10 +14,23 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 	private static final String SELECT_ALL = "Select ARTICLES_VENDUS.nom_article, ARTICLES_VENDUS.description, ARTICLES_VENDUS.date_debut_encheres, ARTICLES_VENDUS.date_fin_encheres, ARTICLES_VENDUS.prix_initial, ARTICLES_VENDUS.no_utilisateur FROM ENCHERES"
 			+ " INNER JOIN ARTICLES_VENDUS on ENCHERES.no_article = ARTICLES_VENDUS.no_article;";
 	private static final String DELETE = "DELETE FROM ENCHERES WHERE no_utilisateur=?";
+	private static final String INSERT = "INSERT INTO ENCHERES VALUES (?,?,?,?)";
 
 	@Override
-	public void insert(Enchere object) {
-
+	public void insert(Enchere enchere) {
+		PreparedStatement preparedStatement = null;
+		try (Connection connection = ConnectionProvider.getConnection()) {
+			preparedStatement = connection.prepareStatement(INSERT);
+			preparedStatement.setInt(1, enchere.getEncherisseur().getNoUtilisateur());
+			preparedStatement.setInt(2, enchere.getArticleVendu().getNoArticle());
+			preparedStatement.setObject(3, enchere.getDateEnchere());
+			preparedStatement.setInt(4, enchere.getMontantEnchere());
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResources(preparedStatement, null);
+		}
 	}
 
 	@Override
@@ -28,24 +40,7 @@ public class EnchereDAOJdbcImpl implements EnchereDAO {
 
 	@Override
 	public List<Enchere> selectAll() {
-		Statement statement = null;
-		ResultSet resultSet = null;
-		List<Enchere> listeEncheres = new ArrayList<>();
-		try (Connection connection = ConnectionProvider.getConnection()) {
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery(SELECT_ALL);
-
-//			while (resultSet.next()) {
-//				listeEncheres.add(new Enchere(resultSet.getDate("date_debut_encheres").toLocalDate(),
-//						resultSet.getInt("prix_initial"), (Utilisateur) resultSet.getObject("no_utilisateur"),
-//						(ArticleVendu) resultSet.getObject("no_article")));
-//			}
-		} catch (SQLException throwables) {
-			throwables.printStackTrace();
-		} finally {
-			closeResources(statement, resultSet);
-		}
-		return listeEncheres;
+		return null;
 	}
 
 	@Override

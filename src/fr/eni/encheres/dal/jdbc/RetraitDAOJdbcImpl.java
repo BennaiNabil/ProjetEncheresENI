f5b5f83196a9d3b8acbb6d3b9776408050f97a04
Léa V. <lvangheluwe@gmail.com>
2021-02-05 16:20:52 +0100

    Article par id (qui marche \o/)

--- src/fr/eni/encheres/dal/jdbc/RetraitDAOJdbcImpl.java
+++ src/fr/eni/encheres/dal/jdbc/RetraitDAOJdbcImpl.java
@@ -3,13 +3,35 @@ package fr.eni.encheres.dal.jdbc;
 import fr.eni.encheres.bo.Retrait;
 import fr.eni.encheres.dal.RetraitDAO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class RetraitDAOJdbcImpl implements RetraitDAO {
+	private static final String INSERT = "INSERT INTO RETRAITS VALUES(?,?,?,?)";
+	
     @Override
-    public void insert(Retrait object) {
+    public void insert(Retrait retrait) {
+    	PreparedStatement preparedStatement = null;
+    	ResultSet resultSet = null;
+    	try (Connection connection = ConnectionProvider.getConnection()) {
+    	preparedStatement = connection.prepareStatement(INSERT);
+    	preparedStatement.setInt(1, retrait.getArticleVendu().getNoArticle());
+    	preparedStatement.setString(2, retrait.getRue() );
+    	preparedStatement.setString(3, retrait.getCodePostal());
+    	preparedStatement.setString(4, retrait.getVille());
 
-    }
+    	preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			closeResources(preparedStatement, resultSet);
+		}
+	}
+
 
     @Override
     public Retrait selectById(int id) {
@@ -30,4 +52,21 @@ public class RetraitDAOJdbcImpl implements RetraitDAO {
     public void delete(Retrait object) {
 
     }
+    
+    private void closeResources(Statement preparedStatement, ResultSet resultSet) {
+		if (resultSet != null) {
+			try {
+				resultSet.close();
+			} catch (SQLException throwables) {
+				throwables.printStackTrace();
+			}
+		}
+		if (preparedStatement != null) {
+			try {
+				preparedStatement.close();
+			} catch (SQLException throwables) {
+				throwables.printStackTrace();
+			}
+		}
+	}
 }

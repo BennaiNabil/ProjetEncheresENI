6368b9e27f193ce656e51bc9a3a1fc5a8a38cfd4
Bennai Nabil <40771404+NabilBennai@users.noreply.github.com>
2021-02-08 14:12:08 +0100

    Affichage des encheres en cours par catégorie (moche)

--- WebContent/WEB-INF/jsp/PageAccueilAnonyme.jsp
+++ WebContent/WEB-INF/jsp/PageAccueilAnonyme.jsp
@@ -26,20 +26,20 @@
 </head>
 <body>
 
-	<%@include file="FragmentHeader.jspf"%>
+	<%-- <%@include file="FragmentHeader.jspf"%>
 	<%@include file="../BandeauErreurs.html"%>
-
+ --%>
 	<form
 		method="POST"
 		action="<%=request.getContextPath()%>/AfficherEncheresCourantes">
 		<select
-			name="cate"
-			id="cate">
+			name="categorie"
+			id="categorie">
 			<option value="">-- Sélectionnez une catégorie --</option>
 			<c:forEach
 				items="${listeCategories}"
-				var="categorie">
-				<option value="${categorie.getNoCategorie()}">${categorie.getLibelle()}</option>
+				var="cate">
+				<option value="${cate.getNoCategorie()}">${cate.getLibelle()}</option>
 			</c:forEach>
 		</select>
 		<button
@@ -49,7 +49,7 @@
 
 	<ul>
 		<c:forEach
-			items="${listeEncheres}"
+			items="${listeEncheresEnCours}"
 			var="enchere">
 			<li>${enchere.toString() }</li>
 		</c:forEach>
@@ -57,6 +57,7 @@
 	<%@include file="FragmentFooter.jspf"%>
 
 
+	
 
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
---
+++ src/fr/eni/encheres/servlets/AfficherEncheresCourantes.java
@@ -0,0 +1,46 @@
+package fr.eni.encheres.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.encheres.bll.CategorieManager;
+import fr.eni.encheres.bll.EncheresManager;
+import fr.eni.encheres.bo.ArticleVendu;
+import fr.eni.encheres.bo.Categorie;
+
+public class AfficherEncheresCourantes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+
+		int noCategorie = Integer.parseInt(request.getParameter("categorie"));
+
+		CategorieManager categorieManager = new CategorieManager();
+		EncheresManager encheresManager = new EncheresManager();
+
+		Categorie categorieChoisie = categorieManager.selectCategorieById(noCategorie);
+		List<ArticleVendu> listeEncheresEnCours = encheresManager
+				.recueprerEncheresEnCoursParCategorie(categorieChoisie.getLibelle());
+
+		request.setAttribute("listeEncheresEnCours", listeEncheresEnCours);
+
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/PageAccueilAnonyme.jsp");
+		rd.forward(request, response);
+	}
+
+}
